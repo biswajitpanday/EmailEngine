2a909a24d8d0ff13dec59ae11138ff72d391b09d
biswajitpanday <biswajitmailid@gmail.com>
2024-06-16 20:32:11 +0000

    Issue Fix And Configure class-transformer & joi schema validator.

---
+++ src/infrastructure/persistence/validations/BaseValidationSchema.ts
@@ -0,0 +1,7 @@
+import Joi from 'joi';
+
+export const baseValidationSchema = Joi.object({
+  _id: Joi.string().optional().regex(/^[0-9a-fA-F]{24}$/).label('ID'), // MongoDB ObjectID validation
+  createdAt: Joi.date().optional().label('Creation Date'),
+  updatedAt: Joi.date().optional().label('Update Date')
+});
